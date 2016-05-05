66a3154052de13ea7a4ab9b80a02b49c4d8397ac
smith750 <jksmith@kuali.co>
2016-05-05 13:50:55 -0400

    get teams list to work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../db');
+const _ = require('lodash');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/teams', (req, res, next)=> {
+  knex.select('id','name').from('teams')
+      .then(rows => {
+          const teams = _.map(rows, (row) => {
+              return { 'id': row['id'], 'name': row['name'] }
+          });
+          res.send(teams);
+      });
 });
 
 module.exports = router;
