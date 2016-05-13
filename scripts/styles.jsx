ce9dbebe728ea0f24e9f50c9c677495dad462b27
smith750 <jksmith@kuali.co>
2016-05-13 18:34:05 -0400

    build participants page, and refactor out EventAware component as well as styles

---
+++ scripts/styles.jsx
@@ -0,0 +1,18 @@
+const mainStyle = {
+    fontFamily: "Gill Sans, sans-serif",
+    fontSize: "12px"
+};
+
+const tableCellStyle = {
+    padding: "5 10 5 10"
+};
+
+const headerStyle = {
+    fontSize: "1.25em"
+};
+
+module.exports = {
+    mainStyle: mainStyle,
+    headerStyle: headerStyle,
+    tableCellStyle: tableCellStyle
+};
